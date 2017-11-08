884ab199ff25cfaec5657fc93d7702860b1cfb4a
Mary Lynn Tom <vivicca28@gmail.com>
2017-11-07 17:23:54 -0800

    Update derp.js

--- derp.js
+++ derp.js
@@ -1,9 +1,7 @@
-var paper = document.getElementById("paper");
+// Get the canvas element from the document
+var canvas = document.getElementById("paper");
 
-var apple = {
-    
-};
-
-var potato = {
-    
+var Player = function (xStart, yStart, ) {
+    this.xStart = ;
+    this.yStart = ;
 };
