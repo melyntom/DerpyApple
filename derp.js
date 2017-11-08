ef1791eb8415afd268c2f134890affa8309416c6
Mary Lynn Tom <vivicca28@gmail.com>
2017-11-07 17:34:34 -0800

    Update derp.js

--- derp.js
+++ derp.js
@@ -1,7 +1,20 @@
 // Get the canvas element from the document
 var canvas = document.getElementById("paper");
 
-var Player = function (xStart, yStart, ) {
-    this.xStart = ;
-    this.yStart = ;
+// Function for creating a new player
+var Player = function (xStart, yStart, xCurrent, yCurrent, , score) {
+    this.xStart = xStart;
+    this.yStart = yStart;
+    this.xCurrent = xCurrent;
+    this.yCurrent = yCurrent;
+    this.
+    this.score = score;
 };
+
+// Create a new player, Derpy Apple
+var apple = new Player(null, null, null, null, , null);
+console.log(apple);
+
+// Create a new player, Happy Potato
+var potato = new Player(null, null, null, null, , null);
+console.log(potato);
