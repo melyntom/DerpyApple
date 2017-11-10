a6dfa199865b87e70b390c0c1a2330817a9ba735
nicholas-lam <32182594+nicholas-lam@users.noreply.github.com>
2017-11-09 21:51:01 -0800

    Update controls.js

--- js/controls.js
+++ js/controls.js
@@ -1,38 +1,30 @@
 // Keyboard Input for Game
-var model = function (x,y,size,color){
-this.x = x;
-this.y = y;
-this.size = size;
-this.color = color;
-};
-var player;
-document.addEventListener('keydown', function(e) {
-  //console.log(e.keyCode);
-    if(e.keyCode == 37) {
-    }
-    else if(e.keyCode == 39) {
-        //right arrow
-    }
-    else if(e.keyCode == 38) {
-        //up arrow
-    }
-    else if(e.keyCode == 40) {
-       //down arrow
-    }
-});
-var player2;
-document.addEventListener('keydown', function(e) {
-  if (e.keyCode == 87) {
-  //w key
+var canvas = document.querySelector('#canvas');
+var context = canvas.getContext('2d');
+
+var x = 0;
+var y = 0;
+
+context.rect(x, y, 100, 100);
+context.stroke();
+
+function move(e){
+  if(e.keyCode == 39) { //right arrow 
+    x += 5;
   }
-  else if(e.keyCode == 65) {
-    //a key
+  if(e.keyCode == 37) { //left arrow 
+    x -= 5;
   }
-  else if(e.keyCode == 83) {
-    //s key
+  if(e.keyCode == 40) {
+     y += 5;
   }
-  else if(e.keyCode == 68) {
-    //d key
+  if(e.keyCode == 38) {
+    y -= 5;
   }
-          });
+  canvas.width=canvas.width;
+  context.rect(x,y,100,100);
+  context.stroke();
+}
+
+document.onkeydown = move;
                
