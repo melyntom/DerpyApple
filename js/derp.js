4de0eadd87d05719139153b91369b5ddd80a6ffa
Mary Lynn Tom <vivicca28@gmail.com>
2017-11-09 17:49:59 -0800

    Update derp.js

--- js/derp.js
+++ js/derp.js
@@ -1,36 +1,37 @@
+// Object creation
+
+"use strict";
+
 // Get the canvas element from the document
 var canvas = document.getElementById("paper");
 
+// Set canvas size
+canvas.width = 840;
+canvas.height = 560;
+
+// ctx
 var ctx = canvas.getContext("2d");
 
 // Function for creating a new player
-var Player = function (x, y, image, size, score) {
+var Player = function (x, y, image, color, size, score) {
     this.x = x;
     this.y = y;
     this.image = image;
+    this.color = color;
     this.size = size;
     this.score = score;
 };
 
-// Create a new player, Derpy Apple
-var derp = new Player(10, 10, 10, 10, 10);
+// Create new player, Derpy Apple
+var derp = new Player(0, 275, null, "#c11313", 10, 10);
 console.log(derp);
 
-// Create a new player, Happy Potato
-var potato = new Player(null, null, null, null);
+// Create new player, Happy Potato
+var potato = new Player(835, 275, null, "#e2bf93", 10, 10);
 console.log(potato);
 
+// Draw players
 Player.prototype.draw = function (ctx) {
-    ctx.fillStyle = "blueviolet";
-    ctx.fillRect (this.x, this.y, this.size, this.size);
-}
-
-function update() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    derp.draw (ctx);
-    derp.x++;
-    
-    requestAnimationFrame(update);
-}
-
-update();
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+};
