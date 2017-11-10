727a5293e613392e53befbd571fb8aff9a67225d
Mary Lynn Tom <vivicca28@gmail.com>
2017-11-09 17:09:47 -0800

    Update movement.js

--- js/movement.js
+++ js/movement.js
@@ -1,1 +1,10 @@
 // Movement loops code
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    derp.draw (ctx);
+    derp.x++;
+    
+    requestAnimationFrame(update);
+}
+
+update();
