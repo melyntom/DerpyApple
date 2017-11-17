559355b2ec5334465c0c61461d59308f3a0e27aa
Mary Lynn Tom <vivicca28@gmail.com>
2017-11-16 17:36:12 -0800

    Update start.js

--- js/start.js
+++ js/start.js
@@ -1,1 +1,15 @@
 // Main Menu Code - Starting Screen
+
+"use strict";
+
+// Get the canvas element from the document
+var canvas = document.getElementById("paper");
+
+// Set canvas size
+canvas.width = 840;
+canvas.height = 560;
+
+// ctx
+var ctx = canvas.getContext("2d");
+
+
