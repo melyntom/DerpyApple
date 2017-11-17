00b4350251286599db44174bc38c900af77ea09e
nicholas-lam <32182594+nicholas-lam@users.noreply.github.com>
2017-11-16 16:23:24 -0800

    Update controls.js

--- js/controls.js
+++ js/controls.js
@@ -1,95 +1,49 @@
 // Keyboard Input for Game
 
-//divides canvas by 10
-var gridSize = 10;
-
-// game speed in a set interval
-var gameSpeed = 100;
-let interval;
-
-// creating the derpy apple 'player' 
-var derp = {
-  init: function(w, h, size){
-  this.dir = "d";
-  this.size = size;
-    
-    //center of the screen
-    var center = {
-      x: (w/ 2 / this.size | 0) *this.size,
-      y: (w/ 2/ this.size | 0) * this.size
-    };
-    
-    
-  this.body = [];
-    for (let i = 0; i < 3; i++) {
-      this.body.push({
-        x: center.x,
-        y: center.y - i * this.size
-      });
-    }
-  },
-  
-  //in need of a drawing function of the player
-  
-  //changes the direction of the apple
-  changeDir: function (dir) {
-    if (dir == "u") {
-      this.dir = dir;
-    }  
-    if (dir == "d") {
-      this.dir = dir;
-    } 
-    if (dir == "r") {
-      this.dir = dir;
-    } 
-    if (dir == "l") {
-      this.dir = dir;
-    } 
-},  
- 
-  //move function the apple where the apple is pointing towards
-  move: function(w, h) { //head of the apple
-  var head = {
-  x: this.body[0].x,
-  y: this.body[0].y
+var keyboard = {
+  u: false,
+  downArrow: false,
+  l: false,
+  r: false,
+  w: false,
+  a: false,
+  s: false,
+  down: false
 };
-    if (this.dir == "u") { // 0 is the head
-      this.body[0].y -= this.size;
-    }
-    else if (this.dir == "d") {
-      this.body[0].y += this.size;
-    }
-    else if (this.dir == "l") {
-      this.body[0].x -= this.size;
-    }
-    else if (this.dir == "r") {
-      this.body [0].x += this.size;
-    }
-    this.body.splice(1, 0, head);
-}
-  };
 
-//initizilation function to redraw canvas
-function init() {
-  derp.init(canvas.width, canvas.height, gridSize);
-  
-    document.addEventListener("keydown", function(e) {
-      if (e.keyCode == 37) {
-        derp.changeDir("l");
+ document.addEventListener("keydown", function(e) {
+      if (e.keyCode == 65) {
+        apple.changeDir("l"),
+          keyboard.a = true;
       }
-      else if (e.keyCode == 38) {
-        derp.changeDir("u");
+      else if (e.keyCode == 87) {
+        apple.changeDir("u"),
+          keyboard.w = true;
       }
-      else if (e.keyCode == 39) {
-        derp.changeDir("r");
+      else if (e.keyCode == 68) {
+        apple.changeDir("r"),
+          keyboard.d = true;
       }
-      else if (e.keyCode == 40) {
-        derp.changeDir("d");
+      else if (e.keyCode == 83) {
+        apple.changeDir("d"),
+          keyboard.s = true;
       }
   });
-  
-  //interval for gamespeed
-    clearInterval(interval);
-    interval = setInterval(update, gameSpeed);
-  }
-init();
+    document.addEventListener("keydown", function(e) {
+      if (e.keyCode == 37) {
+        derp.changeDir("l"),
+          keyboard.l = true;
+      }
+      else if (e.keyCode == 38) {
+        derp.changeDir("u"),
+          keyboard.u = true;
+      }
+      else if (e.keyCode == 39) {
+        derp.changeDir("r"),
+          keyboard.r = true;
+      }
+      else if (e.keyCode == 40) {
+        derp.changeDir("d"),
+          keyboard.downArrow = true;
+      }
+  });
