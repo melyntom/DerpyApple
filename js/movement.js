c26ab1de542de0851186263e0fc2548d76e1f17d
nicholas-lam <32182594+nicholas-lam@users.noreply.github.com>
2017-11-28 17:06:12 -0800

    Update movement.js

--- js/movement.js
+++ js/movement.js
@@ -4,8 +4,8 @@ function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     derp.draw (ctx);
     potato.draw (ctx);
-    derp.x++;
-    potato.x--;
+    derp.move();
+    potato.move();
     
     requestAnimationFrame(update);
 }
